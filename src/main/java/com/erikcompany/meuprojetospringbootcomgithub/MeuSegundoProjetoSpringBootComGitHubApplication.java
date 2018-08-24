f9ef9be5e8fb6bcc1d696ca5508236f7a9978dc0
erikflima <erik.alves253@gmail.com>
2018-08-24 20:22:08 -0300

    Last update local to Github.

--- src/main/java/com/erikcompany/meuprojetospringbootcomgithub/MeuSegundoProjetoSpringBootComGitHubApplication.java
+++ src/main/java/com/erikcompany/meuprojetospringbootcomgithub/MeuSegundoProjetoSpringBootComGitHubApplication.java
@@ -3,13 +3,17 @@ package com.erikcompany.meuprojetospringbootcomgithub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Classe principal que de fato executa o projeto.
+
 @SpringBootApplication //Anotacao do Spring Boot que faz tudo acontecer. Inicializa o Spring Boot, componentes, entre um monte de outras coisas.
 public class MeuSegundoProjetoSpringBootComGitHubApplication {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Erik - Essa eh a classe que executa o projeto em si");
+		System.out.println("\n\nErik - Executando o metodo main e apos isso o metodo SpringApplication.run, que roda a aplicacao literalmente!\n\n");
 		
 		SpringApplication.run( MeuSegundoProjetoSpringBootComGitHubApplication.class, args );
+		
+		System.out.println("\n\nErik - Finalizando a execucao do metodo main \n\n");	
 	}
 }
